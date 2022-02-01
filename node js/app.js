2d09e3f8465dcc63235f5152393622d9d6f62758
Winnie Chien <winnie145ch@gmail.com>
2022-02-01 17:07:38 +0800

    0131

---
+++ node js/app.js
@@ -0,0 +1,19 @@
+// section 3-9
+// const fs = require('fs');
+// fs.writeFileSync('note.txt','Hello World╰(*°▽°*)╯');
+// fs.appendFileSync('note.txt','. My name is Winnie(ʘᴥʘ).');
+
+// 3-10
+// require ('./utils.js');
+// const name = 'Winnie';
+// console.log(name);
+// const getNotes = require('./note.js');
+// console.log(getNotes());
+
+// 3-11
+// require validator = require ('validator')
+
+// 3-12
+const chalk = require('chalk');
+console.log(chalk.green('Success!'));
+console.log(chalk.cyan.inverse.bold('Successfulᓚᘏᗢ'));
